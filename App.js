a3403099a2aeae80f73133688cd477316788af6d
Chua Rui Han <chuaruihan8@gmail.com>
2024-11-18 15:09:10 +0800

    setup

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Heart } from 'phosphor-react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
+  const [fontsLoaded] = useFonts ({
+    'bg-xbold': require('./assets/fonts/BricolageGrotesque/BricolageGrotesque-ExtraBold.ttf'),
+    'bg-semibold': require('./assets/fonts/BricolageGrotesque/BricolageGrotesque-SemiBold.ttf'),
+    'bg-bold': require('./assets/fonts/BricolageGrotesque/BricolageGrotesque-Bold.ttf'),
+    'bg-medium': require('./assets/fonts/BricolageGrotesque/BricolageGrotesque-Medium.ttf'),
+    'bg-regular': require('./assets/fonts/BricolageGrotesque/BricolageGrotesque-Regular.ttf'),
+    'bg-light': require('./assets/fonts/BricolageGrotesque/BricolageGrotesque-Light.ttf'),
+    'bg-xlight': require('./assets/fonts/BricolageGrotesque/BricolageGrotesque-ExtraLight.ttf'),
+
+    's-xbold': require('./assets/fonts/Sora/Sora-ExtraBold.ttf'),
+    's-semibold': require('./assets/fonts/Sora/Sora-SemiBold.ttf'),
+    's-bold': require('./assets/fonts/Sora/Sora-Bold.ttf'),
+    's-medium': require('./assets/fonts/Sora/Sora-Medium.ttf'),
+    's-regular': require('./assets/fonts/Sora/Sora-Regular.ttf'),
+    's-thin': require('./assets/fonts/Sora/Sora-Thin.ttf'),
+    's-light': require('./assets/fonts/Sora/Sora-Light.ttf'),
+    's-xlight': require('./assets/fonts/Sora/Sora-ExtraLight.ttf'),
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'bg-medium', fontSize: 20 }}>Bricolage Grotesque Header</Text>
+      <Text style={{ fontFamily: 's-regular', fontSize: 16, color: '#9E9E9E', }}>Sora Body font</Text>
+      <Heart size={40} color="blue" />
+      <Button mode='contained'>
+        React Native Paper Button
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
